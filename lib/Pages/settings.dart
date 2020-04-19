4554614ef4ce0856411c83fbf95a4f2238fa3d4a
Nithin Sai K J <nithins674@gmail.com>
2020-04-19 17:09:23 +0530

    Change material flat buttons to cupertino buttons

--- lib/Pages/settings.dart
+++ lib/Pages/settings.dart
@@ -5,26 +5,24 @@ import 'loginpage.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class Settings extends StatefulWidget {
-
   final String name;
   final String photoUrl;
   final String email;
 
-  Settings({Key key,@required this.name,this.photoUrl,this.email}): super(key: key);
+  Settings({Key key, @required this.name, this.photoUrl, this.email})
+      : super(key: key);
 
   @override
-  _SettingsState createState() => _SettingsState( name: name, photoUrl: photoUrl, email: email
-  );
-
+  _SettingsState createState() =>
+      _SettingsState(name: name, photoUrl: photoUrl, email: email);
 }
 
 class _SettingsState extends State<Settings> {
-
   final String name;
   final String photoUrl;
   final String email;
 
-  _SettingsState({@required this.name,this.photoUrl,this.email});
+  _SettingsState({@required this.name, this.photoUrl, this.email});
   final GoogleSignIn _gSignIn = GoogleSignIn();
 
   @override
@@ -32,14 +30,21 @@ class _SettingsState extends State<Settings> {
     return Scaffold(
       backgroundColor: Color(0xff111111),
       appBar: CupertinoNavigationBar(
-        middle: Text("Settings", style: TextStyle(color: Colors.white),),
+        middle: Text(
+          "Settings",
+          style: TextStyle(color: Colors.white),
+        ),
         automaticallyImplyLeading: false,
         leading: GestureDetector(
           child: Container(
-            height: 25,
-            width: 40,
-            alignment: Alignment.centerLeft,
-            child: Icon(Icons.arrow_back_ios, color: Colors.grey,size: 20,)),
+              height: 25,
+              width: 40,
+              alignment: Alignment.centerLeft,
+              child: Icon(
+                Icons.arrow_back_ios,
+                color: Colors.grey,
+                size: 20,
+              )),
           onTap: () {
             Navigator.pop(context);
             Navigator.pop(context);
@@ -49,37 +54,36 @@ class _SettingsState extends State<Settings> {
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.fromLTRB(20.0,20.0,20.0,0),
+          padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-             Center(
-               child: CircleAvatar(
-                 backgroundImage: NetworkImage(photoUrl),
-                 radius: 80,
-               ),
-             ),
-             SizedBox(
-                height: 10,
+              Center(
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(photoUrl),
+                  radius: 80,
+                ),
               ),
-             Divider(
-               color: Colors.grey,
-               height: 30,
-             ),
               SizedBox(
                 height: 10,
               ),
-              Text(name,
-                style: TextStyle(color: Colors.white,
-                    fontSize: 20),
+              Divider(
+                color: Colors.grey,
+                height: 30,
               ),
-              
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                name,
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
               SizedBox(
                 height: 30,
               ),
-              Text(email,
-                style: TextStyle(color: Colors.white,
-                    fontSize: 20),
+              Text(
+                email,
+                style: TextStyle(color: Colors.white, fontSize: 20),
               ),
               SizedBox(
                 height: 30,
@@ -87,7 +91,7 @@ class _SettingsState extends State<Settings> {
               SizedBox(
                 child: Hero(
                   tag: 'dasher',
-                                  child: FlatButton(
+                  child: FlatButton(
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(5),
                     ),
@@ -96,51 +100,54 @@ class _SettingsState extends State<Settings> {
                       showDialog(
                           context: context,
                           builder: (_) => CupertinoAlertDialog(
-                            title: Text("Sign Out?",
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold
-                              ),),
-                            content: Text("\nAre you sure you want to sign out?\n\nYou will no longer be able to access Istriwaala",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 14)
-                            ),
-                            actions: <Widget>[
-                              FlatButton(
-                                child: Text("Yes",
-                                    style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 14)),
-                                onPressed: () async {
-                                  SharedPreferences prefs = await SharedPreferences.getInstance();
-                                  prefs.remove('userEmail');
-                                  prefs.remove('userName');
-                                  prefs.remove('userPhoto');
-                                  prefs.setBool('login', false);
-                                  _gSignIn
-                                      .signOut()
-                                      .catchError((error) => print(error));
-                                  print('Signed out');
-                                  Navigator.pushAndRemoveUntil(context, CupertinoPageRoute(builder: (BuildContext context) => LoginPage()), ModalRoute.withName('/'));
-                                },
-                              ),
-                              Hero(
-                                tag: "dasher",
-                                                            child: FlatButton(
-                                  child: Text("Cancel",
-                                      style: TextStyle(
-                                          color: Colors.red,
-                                          fontSize: 14)
-                                  ),
-                                  onPressed: () {
-                                    Navigator.pop(context);
-                                  },
+                                title: Text(
+                                  "Sign Out?",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
                                 ),
-                              ),
-                            ],
-                          ));
+                                content: Text(
+                                    "\nAre you sure you want to sign out?\n\nYou will no longer be able to access Istriwaala",
+                                    style: TextStyle(
+                                        color: Colors.white, fontSize: 14)),
+                                actions: <Widget>[
+                                  CupertinoButton(
+                                    child: Text("Yes",
+                                        style: TextStyle(
+                                            color: Colors.white, fontSize: 14)),
+                                    onPressed: () async {
+                                      SharedPreferences prefs =
+                                          await SharedPreferences.getInstance();
+                                      prefs.remove('userEmail');
+                                      prefs.remove('userName');
+                                      prefs.remove('userPhoto');
+                                      prefs.setBool('login', false);
+                                      _gSignIn
+                                          .signOut()
+                                          .catchError((error) => print(error));
+                                      print('Signed out');
+                                      Navigator.pushAndRemoveUntil(
+                                          context,
+                                          CupertinoPageRoute(
+                                              builder: (BuildContext context) =>
+                                                  LoginPage()),
+                                          ModalRoute.withName('/'));
+                                    },
+                                  ),
+                                  Hero(
+                                    tag: "dasher",
+                                    child: CupertinoButton(
+                                      child: Text("Cancel",
+                                          style: TextStyle(
+                                              color: Colors.red, fontSize: 14)),
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                      },
+                                    ),
+                                  ),
+                                ],
+                              ));
                     },
                     child: Text(
                       "Sign Out",
@@ -153,18 +160,22 @@ class _SettingsState extends State<Settings> {
                   ),
                 ),
               ),
-               Divider(
-               color: Colors.grey,
-               height: 30,
-             ),
-             SizedBox(height: MediaQuery.of(context).size.height* .3,),
-             Center(
-               child: CupertinoButton(
-                 child: Text("I am a customer", 
-                 style: TextStyle(color: Colors.grey[600]),),
-                 onPressed: () {},
-               ),
-             )
+              Divider(
+                color: Colors.grey,
+                height: 30,
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * .3,
+              ),
+              Center(
+                child: CupertinoButton(
+                  child: Text(
+                    "I am a customer",
+                    style: TextStyle(color: Colors.grey[600]),
+                  ),
+                  onPressed: () {},
+                ),
+              )
             ],
           ),
         ),
